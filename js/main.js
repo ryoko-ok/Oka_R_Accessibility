2c4128304b955cbfb355bb432be53b5aba7b9807
Ryoko Oka <ryooo.kooo.22@gmail.com>
2021-04-08 14:28:14 -0400

    changed js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
-var videoPlayer=document.getElementById("videoPlayer"),
-links=videoPlayer.getElementsByTagName('a');
+var mediaPlayer=document.getElementById("mediaPlayer"),
+    links=videoPlayer.getElementsByTagName('a');
+    inks=audioPlayer.getElementsByTagName('a');
+
 for(var i=0; i<links.length; i++) {
   links[i].onclick=handler;
 }
 
 function handler(e) {
 	e.preventDefault();
-	videotarget = this.getAttribute("href");
-	filename = videotarget.substr(0, videotarget.lastIndexOf('.')) || videotarget;
-	video = document.querySelector("#videoPlayer video");
-	video.removeAttribute("controls");
-	video.removeAttribute("poster");
-	source = document.querySelectorAll("#videoPlayer video source");
-	source[0].src = filename + ".mp4";
-	video.load();
-	video.play();    
+	targetHref = this.getAttribute("href");
+    video = document.querySelector("#mediaPlayer video");
+    audio = document.getElementById("#mediaPlayer audio")
+
+    // Pause anything that is currently playing
+    audio.pause();
+    video.pause();
+
+    if (targetHref.endsWith('mp3')) {
+        // Audio
+        audio.src = targetHref;
+        audio.play();
+    } else {
+        // Video
+        filename = targetHref.substr(0, targetHref.lastIndexOf('.')) || targetHref;
+        video.removeAttribute("controls");
+	      video.removeAttribute("poster");
+        source = document.querySelectorAll("#mediaPlayer video source");
+	      source[0].src = filename + ".mp4";
+        video.load();
+	      video.play();
+    }    
 }
