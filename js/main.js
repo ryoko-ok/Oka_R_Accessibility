827562ad3a522459e30f88e3a90266be9de23d44
Ryoko Oka <ryooo.kooo.22@gmail.com>
2021-04-08 13:53:55 -0400

    changed js

--- js/main.js
+++ js/main.js
@@ -1,91 +1,18 @@
-(() => {
-    // make a reference to all of the buttons
-    const playButtons = document.querySelectorAll('.playButton'),
-          pauseButtons = document.querySelectorAll('.pauseButton')
-          rwButtons = document.querySelectorAll('.rwButton'),
-          audioElement = document.querySelector('audio');
-  
-    let globalPaused = false;
-  
-    // play the son associated with the button (itsdata-trackref attribute)
-    function playTrack() {
-      // try to fix pause play //
-      // a return statement kills your code execution here - don't go past this point
-      if (globalPaused) {
-        console.log('paused');
-        // if our audio is paused, then just play te track and exit
-        resumeTrack();
-        return;
-      }
-  
-      //debugger;
-      let audioSource = this.dataset.trackref;
-  
-      // set the audio source
-      audioElement.src = `audio/${audioSource}.mp3`;
-  
-      // load and play it
-      audioElement.load();
-      audioElement.play();
-      //playTrack();
-    }
-  
-    function resumeTrack() {
-      globalPaused = false;
-      audioElement.play();
-    }
-  
-    function pauseTrack() {
-      audioElement.pause();
-      globalPaused = true;
-    }
-  
-    function rwTrack() {
-      audioElement.currentTime = 0;
-    }
-  
-  
-    // process the playButtons and add some event handling
-    playButtons.forEach(button => button.addEventListener("click", playTrack));
-    pauseButtons.forEach(button => button.addEventListener("click", pauseTrack));
-    rwButtons.forEach(button => button.addEventListener("click", rwTrack));
+var videoPlayer=document.getElementById("videoPlayer"),
+links=videoPlayer.getElementsByTagName('a');
+for(var i=0; i<links.length; i++) {
+  links[i].onclick=handler;
+}
 
-
-    // Video ---------------------------------
-
-  let playVideoButton = document.querySelector('.playVideoButton'),
-      pauseVideoButton = document.querySelector('.pauseVideoButton'),
-      rwVideoButton = document.querySelector('.rwVideoButton');
-      videoElement = document.querySelector('video');
-
-
-      let videoSource = this.dataset.videoref;
-  
-      // set the video source
-      videoElement.src = `video/${videoSource}.mp4`;
-
-      // load the lightbox video when it opens
-      videoElement.load();
-      // play the lightbox video when it opens
-      VideoElement.play();
-
-      function playVideo() {
-        VideoElement.play();
-    }
-  
-    function pauseVideo() {
-        VideoElement.pause();
-    }
-  
-    function rwVideo() {
-      VideoElement.currentTime = 0;
-    }
-
-    playVideoButton.addEventListener("click", playVideo);
-
-    pauseVideoButton.addEventListener("click", pauseVideo);
-
-    rwVideoButton.addEventListener("click", rwVideo);
-  
-  })();
-  
+function handler(e) {
+	e.preventDefault();
+	videotarget = this.getAttribute("href");
+	filename = videotarget.substr(0, videotarget.lastIndexOf('.')) || videotarget;
+	video = document.querySelector("#videoPlayer video");
+	video.removeAttribute("controls");
+	video.removeAttribute("poster");
+	source = document.querySelectorAll("#videoPlayer video source");
+	source[0].src = filename + ".mp4";
+	video.load();
+	video.play();    
+}
