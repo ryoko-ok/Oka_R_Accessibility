e4e9d85123f00d89d41282b32f25a88d3a0cb72e
Ryoko Oka <ryooo.kooo.22@gmail.com>
2021-04-09 22:55:00 -0400

    adding artwork

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
-var mediaPlayer=document.getElementById("mediaPlayer"),
-    links=videoPlayer.getElementsByTagName('a');
-    inks=audioPlayer.getElementsByTagName('a');
-
-for(var i=0; i<links.length; i++) {
-  links[i].onclick=handler;
+function playVideo(num) {
+    console.log(num);
+    tag = document.getElementById("video");
+    switch (num) {
+        case 1:
+            tag.src = "video/movie60sTiffany.mp4";
+            break;
+        case 2:
+            tag.src = "video/tv90sDharmaandgreg.mp4";
+            break;
+        case 3:
+            tag.src = "audio/music50sIwannabelovedbyyou.mp3";
+            break;
+    }
 }
 
-function handler(e) {
-	e.preventDefault();
-	targetHref = this.getAttribute("href");
-    video = document.querySelector("#mediaPlayer video");
-    audio = document.getElementById("#mediaPlayer audio")
+var mybutton = document.getElementById("backtotop");
+        
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-    // Pause anything that is currently playing
-    audio.pause();
-    video.pause();
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-    if (targetHref.endsWith('mp3')) {
-        // Audio
-        audio.src = targetHref;
-        audio.play();
-    } else {
-        // Video
-        filename = targetHref.substr(0, targetHref.lastIndexOf('.')) || targetHref;
-        video.removeAttribute("controls");
-	      video.removeAttribute("poster");
-        source = document.querySelectorAll("#mediaPlayer video source");
-	      source[0].src = filename + ".mp4";
-        video.load();
-	      video.play();
-    }    
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
